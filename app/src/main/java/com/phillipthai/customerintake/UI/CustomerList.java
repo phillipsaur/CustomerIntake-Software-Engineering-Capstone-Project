beb22eb6e1d458b39cbd656e8ff698f7e970344b
phillipsaur <phillipdthai@gmail.com>
2024-08-09 14:33:51 -0700

    Initial commit push

---
+++ app/src/main/java/com/phillipthai/customerintake/UI/CustomerList.java
@@ -0,0 +1,21 @@
+package com.phillipthai.customerintake.UI;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.phillipthai.customerintake.R;
+
+public class CustomerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_customer_list);
+    }
+}
