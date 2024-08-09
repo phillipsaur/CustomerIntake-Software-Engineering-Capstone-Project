8b83d5ca48fb75ce985b90df903f3b9df28e6ad6
phillipsaur <phillipdthai@gmail.com>
2024-08-09 16:51:46 -0700

    Added Recycler View, FAB, and FAB functionality to CustomerList

--- app/src/main/java/com/phillipthai/customerintake/UI/CustomerList.java
+++ app/src/main/java/com/phillipthai/customerintake/UI/CustomerList.java
@@ -1,6 +1,8 @@
 package com.phillipthai.customerintake.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.phillipthai.customerintake.R;
 
 public class CustomerList extends AppCompatActivity {
@@ -17,5 +20,14 @@ public class CustomerList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_customer_list);
+
+        FloatingActionButton fab = findViewById(R.id.customerFAB);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerList.this, CustomerDetails.class);
+                startActivity(intent);
+            }
+        });
     }
 }
