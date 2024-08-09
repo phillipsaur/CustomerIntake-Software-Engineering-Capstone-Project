f4fb7b2dc7903f21f0bf72b960b10e3e1d4609b9
phillipsaur <phillipdthai@gmail.com>
2024-08-09 16:40:58 -0700

    Set up MainActivity and added button to transition to CustomerList

--- app/src/main/java/com/phillipthai/customerintake/UI/MainActivity.java
+++ app/src/main/java/com/phillipthai/customerintake/UI/MainActivity.java
@@ -1,6 +1,9 @@
 package com.phillipthai.customerintake.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CustomerList.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
