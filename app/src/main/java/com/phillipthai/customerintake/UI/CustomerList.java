e18166be10e9377fcd2b79cd5dd8f1ea73e58ab7
phillipsaur <phillipdthai@gmail.com>
2024-08-10 00:25:55 -0700

    Cleaned up code

--- app/src/main/java/com/phillipthai/customerintake/UI/CustomerList.java
+++ app/src/main/java/com/phillipthai/customerintake/UI/CustomerList.java
@@ -6,9 +6,6 @@ import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.phillipthai.customerintake.R;
